42eb47c1d70fadb6d957e28d660c1ce413207362
Your Name <you@example.com>
2021-02-04 23:52:14 -0600

    Test Commit

---
+++ src/lambda/workout/Feature1.java
@@ -0,0 +1,9 @@
+public class Feature1{
+    public static void main(String []args){
+
+        List<String> list = new ArrayList<>();
+        list.add("Murugan");
+        list.add("Vijay");
+        list.add("Hari");
+    }
+}
